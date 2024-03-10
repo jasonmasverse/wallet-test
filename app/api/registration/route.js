42a5120a61973586f19fc0d59a35fba51e0f26fd
Masverse Chan <Chan.VS@masverse.com.my>
2024-03-10 12:47:25 +0800

    remove create wallet

--- app/api/registration/route.js
+++ app/api/registration/route.js
@@ -6,21 +6,12 @@ export async function POST(req) {
     const data = await req.json()
 
     try {
-        const res = await axios(process.env.WALLET, {
-            method: "POST",
-            headers: {
-                'Authorization': process.env.API,
-                'Content-Type': 'application/json'
-            }
-        });
-        if (res.data.status === 'success') {
-            const conn = await connect();
-            const [results, fields] = await conn.execute(
-                'INSERT into register (email,name) values (?,?)',
-                [data.email, data.name]
-            );
-            
-        }
+        const conn = await connect();
+        const [results, fields] = await conn.execute(
+            'INSERT into register (email,name) values (?,?)',
+            [data.email, data.name]
+        );
+
 
     } catch (error) {
         return NextResponse.json({
@@ -32,8 +23,8 @@ export async function POST(req) {
     return NextResponse.json({
         status: "success",
         data: {
-            private: res.data.result.privateKey,
-            address: res.data.result.address
+            email: data.email,
+            name: data.name
         }
     })
 }
