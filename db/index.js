46321529b4318749f2ed751b612e59bf2292fe41
Masverse Chan <Chan.VS@masverse.com.my>
2024-03-11 11:01:31 +0800

    use connection pool

--- db/index.js
+++ db/index.js
@@ -7,11 +7,18 @@ export async function connect() {
     return pool;
   }
 
-  pool = await mysql.createConnection({
+  pool = await mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password:process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10, 
+    idleTimeout: 60000,
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
   });
 
 
