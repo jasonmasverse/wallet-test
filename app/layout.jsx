a7cbab15a80dc008361bcfeb9c27e098839f77dc
jason.aw <jason.ahw@masverse.com.my>
2024-03-12 17:13:26 +0800

    rename

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,7 +5,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Maschain",
+  title: "MasWallet",
   description: "Maschain Wallet Experience",
 };
 
